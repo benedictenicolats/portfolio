dcf0b78ab427b5ac1e91833c71aadc9113a3da59
benedictenicolats <nicolatsbenedicte@gmail.com>
2021-05-04 08:13:33 +0200

    04/05

---
+++ js/popup.js
@@ -0,0 +1,8 @@
+script
+      function openForm() {
+        document.getElementById("modal-01").style.display="block";
+      }
+      
+      function closeForm() {
+        document.getElementById("modal-01").style.display="none";
+      }
---
+++ popup.js
@@ -0,0 +1,8 @@
+script
+      function opendiv() {
+        document.getElementById("modal-01").style.display="block";
+      }
+      
+      function closediv() {
+        document.getElementById("modal-01").style.display="none";
+      }
